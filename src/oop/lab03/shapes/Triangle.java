367ca515c43808920c12a38861cef702c88a2642
Matteo <matteo.violani@studio.unibo.it>
2020-11-13 00:16:36 +0100

    add fields and some correctiong

--- src/oop/lab03/shapes/Triangle.java
+++ src/oop/lab03/shapes/Triangle.java
@@ -1,31 +1,31 @@
 package oop.lab03.shapes;
 
-import java.math.*;
 import oop.lab03.shapes.interfaces.Polygon;
 
 public class Triangle implements Polygon{
 
     private static final int TRI_EDGES = 3;
-    private static final double POWER_EXP = 2;
+    private static final double DIVIDER_AREA_TRI = 2;
+    private final double b;
+    private final double h;
     private final double edge1;
     private final double edge2;
-    private final double edge3;
 
-    public Triangle(double edge1, double edge2, double edge3) {
+    public Triangle(double b, double h, double edge1, double edge2) {
+        this.b = b;
+        this.h = h;
         this.edge1 = edge1;
         this.edge2 = edge2;
-        this.edge3 = edge3;
     }
 
     @Override
     public double getArea() {
-        return (Math.sqrt(Math.pow(this.edge1, POWER_EXP) + 
-                Math.pow(this.edge2/2, POWER_EXP)) * this.edge3);
+        return (this.b * this.h) / DIVIDER_AREA_TRI;
     }
 
     @Override
     public double getPerimeter() {
-        return (this.edge1 + this.edge2 + this.edge3);
+        return (this.edge1 + this.edge2 + this.b);
     }
 
     @Override
